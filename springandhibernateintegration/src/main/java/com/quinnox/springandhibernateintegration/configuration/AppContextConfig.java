244b4787381ee09d79f5287c10bd72f60dc1e38b
gkdlab <gaurav10081992@gmail.com>
2019-12-05 16:34:47 +0530

    integration

---
+++ springandhibernateintegration/src/main/java/com/quinnox/springandhibernateintegration/configuration/AppContextConfig.java
@@ -0,0 +1,8 @@
+package com.quinnox.springandhibernateintegration.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContextConfig {
+
+}
